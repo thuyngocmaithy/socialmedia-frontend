c838f10ee8860fbd6c9871db7ce271ebc5cd07f2
haile38 <lethanhhai586@gmail.com>
2023-11-14 15:15:27 +0700

    stomp

--- src/context/StompContext.js
+++ src/context/StompContext.js
@@ -1,16 +1,34 @@
-import { createContext } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { Stomp } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
 
 const StompContext = createContext(null);
 
 function StompProvider({ children }) {
-    // HANDLE SEND MESSAGE
-    var socket = new SockJS('http://localhost:8080/ws');
-    let stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame) {
-        console.log('Connected: ' + frame);
-    });
+    const [stompClient, setStompClient] = useState(null);
+
+    useEffect(() => {
+        // Chỉ kết nối khi stompClient chưa được thiết lập
+        if (!stompClient) {
+            const socket = new SockJS('http://localhost:8080/ws');
+            const client = Stomp.over(socket);
+
+            client.connect({}, function (frame) {
+                console.log('Connected: ' + frame);
+            });
+
+            setStompClient(client);
+        }
+
+        // Clean up function
+        return () => {
+            // Ngắt kết nối khi component unmount
+            if (stompClient) {
+                stompClient.disconnect();
+            }
+        };
+    }, [stompClient]); // useEffect sẽ chạy lại khi stompClient thay đổi
+
     return <StompContext.Provider value={stompClient}>{children}</StompContext.Provider>;
 }
 
